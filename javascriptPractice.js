8ee581bcfdbe1b15808816cbe057d519034cfed4
Dustin Lunoe <dlunoe@yahoo.com>
2019-11-27 20:25:44 -0700

    repeatStr challenge done

--- javascriptPractice.js
+++ javascriptPractice.js
@@ -5,10 +5,20 @@
 
 // console.log(multiply(2,3));
 
-function disemvowel(str) {
-    // var n = str.replace(/a|e|i|o|u|A|E|I|O|U/g, '');
-    var n = str.replace(/a|e|i|o|u/gi, '');
-    return n;
+// function disemvowel(str) {
+//     // var n = str.replace(/a|e|i|o|u|A|E|I|O|U/g, '');
+//     var n = str.replace(/a|e|i|o|u/gi, '');
+//     return n;
+//   }
+
+// console.log(disemvowel("This website is for losers LOL!"));
+
+function repeatStr (n, s) {
+    var str = '';
+    for(i=0; i<n;i++){
+        str = str + s;
+    }
+    return str;
   }
 
-  console.log(disemvowel("This website is for losers LOL!"));
+  console.log(repeatStr(6, "HelloWorld"))
