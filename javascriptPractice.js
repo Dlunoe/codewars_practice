7750ddb562b9e55f6077f341f2142108a1952ad7
Dustin Lunoe <dlunoe@yahoo.com>
2019-11-27 19:48:46 -0700

    disemvowel challenge

--- javascriptPractice.js
+++ javascriptPractice.js
@@ -1,6 +1,13 @@
-function multiply(a, b){
-    // console.log(a, b);
-    return a * b 
-}
+// function multiply(a, b){
+//     // console.log(a, b);
+//     return a * b 
+// }
 
-console.log(multiply(2,3));
+// console.log(multiply(2,3));
+
+function disemvowel(str) {
+    var n = str.replace(/a|e|i|o|u|A|E|I|O|U/g, '');
+    return n;
+  }
+
+  console.log(disemvowel("This website is for losers LOL!"));
